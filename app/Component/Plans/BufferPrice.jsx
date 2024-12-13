b364fd8f86ee48a6fb8109ebe895224335a53974
oishee <oisheekhan90@gmail.com>
2024-12-13 23:44:46 +0600

    complete buffer price

--- app/Component/Plans/BufferPrice.jsx
+++ app/Component/Plans/BufferPrice.jsx
@@ -6,7 +6,7 @@ const BufferPrice = () => {
     return (
         <div className='md:h-[1295px] h-[1478px] w-full bg-[#1A1919]  rounded-2xl text-white'>
         <div className='md:pr-7 md:pl-7 md:pb-7 md:pt-7 p-4'>
-        <h1 className='text-2xl lg:text-4xl text-white font-bold plus-jakarta'>Postiz Pricing</h1>
+        <h1 className='text-2xl lg:text-4xl text-white font-bold plus-jakarta'>Buffer Pricing</h1>
        <div className='md:mt-7 mt-4 flex gap-3 dm-sans items-center'>
        <Image 
                         src="/image/right.png" 
@@ -30,11 +30,11 @@ const BufferPrice = () => {
        
     {/* card */}
     {/* 1 start */}
-<div className="md:w-[566px] w-[287px]   px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
+<div className="md:w-[566px] w-[287px] md:h-[146px] h-[185px]  px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
 <div className="flex md:flex-row flex-col gap-3 justify-between p-4 ">
 <div> 
-<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Standart</h1>
- <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$29 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
+<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Free</h1>
+ <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$0 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
 </div>
 <div>
 <h1 className='mt-2 md:mt-0 text-lg md:text-xl dm-sans text-white'>Social sets: 1</h1>
@@ -43,40 +43,43 @@ const BufferPrice = () => {
 </div>
 </div>
     {/* 2 start */}
-<div className="md:w-[566px] w-[287px]   px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
+<div className="md:w-[566px] w-[287px]  md:h-[146px] h-[185px]  px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
 <div className="flex md:flex-row flex-col gap-3 justify-between p-4 ">
 <div> 
-<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Team</h1>
- <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$39 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
+<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Essentials</h1>
+ <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$6 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
 </div>
 <div>
 <h1 className='mt-2 md:mt-0 text-lg md:text-xl dm-sans text-white'>Social sets: 1</h1>
-<h1 className='md:mt-2 text-lg md:text-xl dm-sans text-white'>Users: unlimited</h1> 
+<h1 className=' text-lg md:text-xl dm-sans text-white'>(additional: $6)</h1>
+<h1 className='md:mt-2 text-lg md:text-xl dm-sans text-white'>Users: 1</h1> 
 </div>
 </div>
 </div>
     {/* 3 start */}
-<div className="md:w-[566px] w-[287px]   px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
+<div className="md:w-[566px] w-[287px] md:h-[146px] h-[185px]   px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
 <div className="flex md:flex-row flex-col gap-3 justify-between p-4 ">
 <div> 
-<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Pro</h1>
- <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$49 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
+<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Team</h1>
+ <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$12 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
 </div>
 <div>
 <h1 className='mt-2 md:mt-0 text-lg md:text-xl dm-sans text-white'>Social sets: 1</h1>
+<h1 className=' text-lg md:text-xl dm-sans text-white'>(additional: $12)</h1>
 <h1 className='md:mt-2 text-lg md:text-xl dm-sans text-white'>Users: unlimited</h1> 
 </div>
 </div>
 </div>
     {/* 4 start */}
-<div className="md:w-[566px] w-[287px]   px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
+<div className="md:w-[566px] w-[287px] md:h-[146px] h-[185px]   px-7 py-4 bg-[#242323] border-2 border-[#353434] rounded-xl shadow-md text-white md:mt-7 mt-3 dark:bg-gray-800">
 <div className="flex md:flex-row flex-col gap-3 justify-between p-4 ">
 <div> 
-<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Ultimate</h1>
- <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$99 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
+<h1 className='font-medium text-lg md:text-xl text-white plus-jakarta'>Agency</h1>
+ <h1 className='mt-3 flex items-center font-semibold plus-jakarta text-3xl md:text-4xl text-white gap-2'>$120 <span className='font-normal text-lg md:text-xl'>/ mo</span> </h1>
 </div>
 <div>
 <h1 className='mt-2 md:mt-0 text-lg md:text-xl dm-sans text-white'>Social sets: 1</h1>
+<h1 className=' text-lg md:text-xl dm-sans text-white'>(additional: $12)</h1>
 <h1 className='md:mt-2 text-lg md:text-xl dm-sans text-white'>Users: unlimited</h1> 
 </div>
 </div>
